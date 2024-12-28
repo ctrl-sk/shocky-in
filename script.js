35ad66b0feefb119182842feca364697a7b73077
ctrl-sk <shashankkumar@ymail.com>
2024-12-28 12:55:44 +0530

    Update

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function setBodyHeight() {
-    const viewportHeight = window.visualViewport
-        ? window.visualViewport.height
-        : window.innerHeight;
+function setLogoContainerHeight() {
+    const viewportHeight = window.visualViewport ? window.visualViewport.height : window.innerHeight;
     const vh = viewportHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
 
-setBodyHeight();
+// Set initial height on load
+setLogoContainerHeight();
 
-window.addEventListener('resize', setBodyHeight);
+// Update on resize events
+window.addEventListener('resize', setLogoContainerHeight);
 
 if (window.visualViewport) {
-    window.visualViewport.addEventListener('resize', setBodyHeight);
+    window.visualViewport.addEventListener('resize', setLogoContainerHeight);
 }
